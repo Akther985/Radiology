1d5b1f0ae6006ce7f09bb75c75bfcb291104bc95
Akther985 <bd.bokul1994@gmail.com>
2023-10-19 12:52:20 -0700

    complite all slider area

--- assets/js/theme.js
+++ assets/js/theme.js
@@ -77,13 +77,47 @@
 				  // instead of a settings object
 				]
 			  });
+			  $('.doctor-slider').slick({
+				dots: false,
+				autoplay: true,
+				infinite: true,
+				button: true,				
+				speed: 300,
+				slidesToShow: 1,
+				slidesToScroll: 1,
+				responsive: [
+				  {
+					breakpoint: 1024,
+					settings: {
+					  slidesToShow: 3,
+					  slidesToScroll: 3,
+					  infinite: true,
+					  dots: true
+					}
+				  },
+				  {
+					breakpoint: 600,
+					settings: {
+					  slidesToShow: 2,
+					  slidesToScroll: 2
+					}
+				  },
+				  {
+					breakpoint: 480,
+					settings: {
+					  slidesToShow: 1,
+					  slidesToScroll: 1
+					}
+				  }
+				  // You can unslick at a given breakpoint now by adding:
+				  // settings: "unslick"
+				  // instead of a settings object
+				]
+			  });
 			
-			/*==== counter active ====*/
+			/*==== counter active ====*/ 
 				
-			$('.counter').counterUp({
-				delay: 20,
-				time: 3000
-			});
+			
 
 })(jQuery);
 
