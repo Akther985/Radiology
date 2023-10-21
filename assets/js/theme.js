5db9d9331b35e5cdc99b89c52d509fa2ec889195
Akther985 <bd.bokul1994@gmail.com>
2023-10-21 12:52:39 -0700

    ok

--- assets/js/theme.js
+++ assets/js/theme.js
@@ -50,23 +50,22 @@
 				slidesToScroll: 1,
 				responsive: [
 				  {
-					breakpoint: 1024,
-					settings: {
-					  slidesToShow: 3,
-					  slidesToScroll: 3,
-					  infinite: true,
-					  dots: true
-					}
-				  },
-				  {
-					breakpoint: 600,
+					breakpoint: 1140,
 					settings: {
 					  slidesToShow: 2,
-					  slidesToScroll: 2
+					  slidesToScroll: 2,
+					  infinite: true,					  
 					}
 				  },
 				  {
-					breakpoint: 480,
+					breakpoint: 960,
+					settings: {
+					  slidesToShow: 1,
+					  slidesToScroll: 1
+					}
+				  },
+				  {
+					breakpoint: 720,
 					settings: {
 					  slidesToShow: 1,
 					  slidesToScroll: 1
@@ -87,23 +86,23 @@
 				slidesToScroll: 1,
 				responsive: [
 				  {
-					breakpoint: 1024,
+					breakpoint: 1140,
 					settings: {
-					  slidesToShow: 3,
-					  slidesToScroll: 3,
+					  slidesToShow: 1,
+					  slidesToScroll: 1,
 					  infinite: true,
-					  dots: true
+					  
 					}
 				  },
 				  {
-					breakpoint: 600,
+					breakpoint: 992,
 					settings: {
-					  slidesToShow: 2,
-					  slidesToScroll: 2
+					  slidesToShow: 1,
+					  slidesToScroll: 1
 					}
 				  },
 				  {
-					breakpoint: 480,
+					breakpoint: 720,
 					settings: {
 					  slidesToShow: 1,
 					  slidesToScroll: 1
